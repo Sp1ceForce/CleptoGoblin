1d54b031a38191c67e683d9a84ef21c60a40e530
Евгений <koronazheka@gmail.com>
2022-12-11 17:32:04 +0300

    Update GuardMoving.cs

--- Assets/Scripts/Guard/GuardMoving.cs
+++ Assets/Scripts/Guard/GuardMoving.cs
@@ -8,7 +8,7 @@ public class GuardMoving : MonoBehaviour
     [SerializeField] List<GameObject> waypoints;
     public System.Action OnPlayerLost;
     int currentIndex = 0;
-
+    [SerializeField] float moveSpeedMultiplier = 1.5f;
     NavMeshAgent navAgent;
 
     GameObject playerRef;
@@ -26,6 +26,7 @@ public class GuardMoving : MonoBehaviour
         isLookingForPlayer=true;
         navAgent.isStopped = false;
         playerRef = playerObject;
+        navAgent.speed = navAgent.speed* moveSpeedMultiplier;
     }
 
     // Update is called once per frame
@@ -58,6 +59,7 @@ public class GuardMoving : MonoBehaviour
         OnPlayerLost?.Invoke();
         isLookingForPlayer = false;
         navAgent.SetDestination(waypoints[currentIndex].transform.position);
+        navAgent.speed = navAgent.speed/moveSpeedMultiplier;
     }
     IEnumerator WaitRoutine()
     {
