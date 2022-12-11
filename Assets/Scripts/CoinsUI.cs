faff16c9889c4ad75ae09273418381285cddaded
Евгений <koronazheka@gmail.com>
2022-12-11 20:05:03 +0300

    e

---
+++ Assets/Scripts/CoinsUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class CoinsUI : MonoBehaviour
+{
+    [SerializeField] TMP_Text CoinsText;
+    [SerializeField] TMP_Text KeysText;
+
+    PlayerItemsController playerItemsController;
+    private void Start()
+    {
+        playerItemsController = FindObjectOfType<PlayerItemsController>();
+        playerItemsController.OnItemsChange += UpdateText;
+    }
+    void UpdateText()
+    {
+        CoinsText.text = playerItemsController.Coins.ToString();
+        KeysText.text = playerItemsController.Keys.ToString();
+    }
+
+}
